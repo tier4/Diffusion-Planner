271538873524b8b00f950ee92da50252da74ab6b
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-12-07 09:34:51 +0900

    Fixed sft_data generation
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_scripts/generate_02_sft_data.sh
+++ ros_scripts/generate_02_sft_data.sh
@@ -3,22 +3,23 @@ set -eux
 
 cd $(dirname $0)
 
-data_root=/mnt/nvme3/sakoda/nas_copy/private_workspace/diffusion_planner/sft_ver1
-
-python3 ./parse_rosbag_for_directory.py \
-    /mnt/nvme2/sakoda/nas_copy/tieriv_dataset/driving_dataset/bag_filtered/2025-10-22 \
-    /mnt/nvme2/sakoda/nas_copy/tieriv_dataset/driving_dataset/bag_filtered/2025-10-29 \
-    /mnt/nvme2/sakoda/nas_copy/tieriv_dataset/driving_dataset/bag_filtered/2025-11-12 \
-    /mnt/nvme2/sakoda/nas_copy/tieriv_dataset/driving_dataset/bag_filtered/2025-11-19 \
-    --save_root $data_root \
-    --step 1 \
-    --limit -1 \
-    --min_frames 1800 \
-    --search_nearest_route 1
+data_root_real=/mnt/nvme3/sakoda/nas_copy/private_workspace/diffusion_planner/preprocessed_ver57_realdata
 
 python3 ../diffusion_planner/util_scripts/create_train_set_path.py \
-    $data_root/2025-10-22 \
-    $data_root/2025-10-29 \
-    $data_root/2025-11-12 \
-    $data_root/2025-11-19 \
-    --save_path $data_root/path_list_train.json
+    $data_root_real/2025-10-29 \
+    $data_root_real/2025-11-12 \
+    $data_root_real/2025-11-19 \
+    --save_path $data_root_real/path_list_train_sft.json
+
+python3 ../diffusion_planner/util_scripts/create_train_set_path.py \
+    $data_root_real/2025-10-29 \
+    $data_root_real/2025-11-12 \
+    $data_root_real/2025-11-19 \
+    --save_path $data_root_real/path_list_train_sft.json
+
+python3 ../diffusion_planner/util_scripts/filter_json.py \
+    $data_root_real/path_list_train_sft.json \
+    --time_filter_jsons \
+        $data_root_real/2025-10-29.json \
+        $data_root_real/2025-11-12.json \
+        $data_root_real/2025-11-19.json \
