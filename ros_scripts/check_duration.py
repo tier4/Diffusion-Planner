5766f9d329a1c9d650868b88e3184fb0caf4864c
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2026-03-17 16:49:10 +0900

    Fixed check_duration.py
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- ros_scripts/check_duration.py
+++ ros_scripts/check_duration.py
@@ -2,12 +2,12 @@ import argparse
 from pathlib import Path
 
 import yaml
-from tqdm import tqdm
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("root_dir", type=Path)
+    parser.add_argument("--depth", type=int, default=1)
     return parser.parse_args()
 
 
@@ -16,23 +16,28 @@ if __name__ == "__main__":
     root_dir = args.root_dir.resolve()
     print(f"{root_dir=}")
 
-    # search "metadata.yaml"
-    metadata_list = sorted(root_dir.glob("**/metadata.yaml"))
+    # root_dirからdepth階層のsubdirを検索
+    subdirs = sorted(root_dir.glob("*/" * args.depth))
+    subdirs = [subdir for subdir in subdirs if subdir.is_dir()]
 
-    total_duration_sec = 0.0
+    for subdir in subdirs:
+        # search "metadata.yaml"
+        metadata_list = sorted(subdir.glob("**/metadata.yaml"))
 
-    for metadata_path in tqdm(metadata_list):
-        metadata = yaml.safe_load(metadata_path.open("r"))
-        nanoseconds = metadata["rosbag2_bagfile_information"]["duration"]["nanoseconds"]
-        seconds = nanoseconds / 1e9
-        total_duration_sec += seconds
+        total_duration_sec = 0.0
 
-    total_sec = int(total_duration_sec)
-    total_hou = total_sec // 3600
-    total_sec -= total_hou * 3600
-    total_min = total_sec // 60
-    total_sec -= total_min * 60
+        for metadata_path in metadata_list:
+            metadata = yaml.safe_load(metadata_path.open("r"))
+            nanoseconds = metadata["rosbag2_bagfile_information"]["duration"]["nanoseconds"]
+            seconds = nanoseconds / 1e9
+            total_duration_sec += seconds
 
-    print(
-        f"Total duration: {total_hou:02d} h {total_min:02d} min {total_sec:02d} sec ({total_duration_sec / 3600:.1f} h)"
-    )
+        total_sec = int(total_duration_sec)
+        total_hou = total_sec // 3600
+        total_sec -= total_hou * 3600
+        total_min = total_sec // 60
+        total_sec -= total_min * 60
+
+        print(
+            f"{subdir.name}\tTotal duration: {total_hou:03d} h {total_min:02d} min {total_sec:02d} sec ({total_duration_sec / 3600:.1f} h)"
+        )
