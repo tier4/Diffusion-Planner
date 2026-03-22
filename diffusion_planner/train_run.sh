9126f9f479d1fcf1bc716cf37b54c7dda71cf844
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2026-03-22 17:31:31 +0900

    Fixed train_run.sh
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- diffusion_planner/train_run.sh
+++ diffusion_planner/train_run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -ux
-cd $(dirname $0)
-
 exp_name=${1}
+TRAIN_SET_LIST=${2}
+VALID_SET_LIST=${3}
+SFT_SET_LIST=${4}
+
+# to convert full paths
+TRAIN_SET_LIST=$(readlink -f $TRAIN_SET_LIST)
+VALID_SET_LIST=$(readlink -f $VALID_SET_LIST)
+SFT_SET_LIST=$(readlink -f $SFT_SET_LIST)
+
+cd $(dirname $0)
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
@@ -14,11 +22,7 @@ export NCCL_DEBUG=INFO
 
 rm -f /tmp/tmp_dist_init
 
-SAVE_DIR="/mnt/nvme0/sakoda/training_result"
-
-TRAIN_SET_LIST="/mnt/nvme2/sakoda/nas_copy/private_workspace/dataset_ver58/path_list_train_with_psim_data.json"
-VALID_SET_LIST="/mnt/nvme2/sakoda/nas_copy/private_workspace/dataset_ver58/path_list_valid.json"
-SFT_SET_LIST="/mnt/nvme2/sakoda/nas_copy/private_workspace/dataset_ver58/path_list_train_sft_in_time_range.json"
+SAVE_DIR="/mnt/nvme/training_result"
 
 # pretraining
 python3 -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone train_predictor.py \
@@ -28,7 +32,7 @@ python3 -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone trai
 --use_wandb True \
 --diffusion_model_type "x_start" \
 --save_dir $SAVE_DIR \
---train_epochs 70 \
+--train_epochs 50 \
 --save_utd 10 \
 2>&1 | tee logs/result_$(date +%Y%m%d_%H%M%S).txt
 
@@ -42,8 +46,8 @@ python3 -m torch.distributed.run --nnodes 1 --nproc-per-node 8 --standalone trai
 --use_wandb True \
 --diffusion_model_type "x_start" \
 --save_dir $SAVE_DIR \
---resume_model_path $SAVE_DIR/$save_dir_name/epoch0060/best_model.pth \
---train_epochs 80 \
+--resume_model_path $SAVE_DIR/$save_dir_name/epoch0040/best_model.pth \
+--train_epochs 50 \
 --save_utd 5 \
 2>&1 | tee logs/result_$(date +%Y%m%d_%H%M%S).txt
 
