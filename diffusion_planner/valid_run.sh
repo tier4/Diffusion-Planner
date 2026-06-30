d4b4d1bad45708834fb78f3d00f865ebdfa97d08
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2026-07-01 08:46:32 +0900

    Added time
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- diffusion_planner/valid_run.sh
+++ diffusion_planner/valid_run.sh
@@ -12,7 +12,8 @@ MODEL_DIR=${1}
 VALID_SET_LIST_PATH=${2}
 MODEL_PATH="$MODEL_DIR/best_model.pth"
 ARGS_JSON_PATH="$MODEL_DIR/args.json"
-SAVE_DIR=$MODEL_DIR/validation_result/predictions
+TIME=$(date +%Y%m%d_%H%M%S)
+SAVE_DIR=$MODEL_DIR/validation_result_$TIME/predictions
 
 rm -f /tmp/tmp_dist_init
 
