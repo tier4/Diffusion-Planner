f99bbf4b4a32ab73a02f115dd0f944e9dfaf0d99
kosuke55 <kosuke.tnp@gmail.com>
2026-08-05 18:50:28 +0900

    add --closed_loop_project_vehicle_map CLI arg

--- diffusion_planner/train_predictor.py
+++ diffusion_planner/train_predictor.py
@@ -252,6 +252,13 @@ def get_args(args_list=None):
         "independently).",
     )
     parser.add_argument(
+        "--closed_loop_project_vehicle_map",
+        type=str,
+        default=_train_config_default("closed_loop_project_vehicle_map"),
+        help="optional JSON file of {project_code_name: vehicle_type_label} for labeling "
+        "--closed_loop_sites_npz_root sites by vehicle type. Empty = no labeling.",
+    )
+    parser.add_argument(
         "--closed_loop_npz_object_modes",
         nargs="+",
         choices=("objects", "noobj"),
